34cc5e925bb77ff903ff8b6093ae238b3839877e
Octopixel40 <82801293+Octopixel40@users.noreply.github.com>
2022-01-24 20:56:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ request.send();
 request.onload = function() {
   const object = request.response
   const randomPost = Math.ceil(Math.random() * object.data.children.length)
-  document.getElementById("cat").innerHTML = `<img src="${object.data.children[randomPost].data.url_overridden_by_dest}">`
+  document.getElementById("cat").innerHTML = `<iframe src="${object.data.children[randomPost].data.url_overridden_by_dest}">`
   document.getElementById("credit").innerHTML = `Posted in r/cats by ${object.data.children[randomPost].data.author}`
 }
